c6223a0fe985236c93c6ab9dd4f25a482ba08e45
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-11 17:38:17 -0500

    Add files via upload

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Header from '../components/header'
 import Footer from '../components/footer'
 import '../styles/index.scss'
+import { container, content } from './layout.module.scss'
 
 const Layout = (props) => {
     return (
-        <div>
-            <Header />
-            {props.children}
+        <div className={container}>
+            <div className={content}>
+                <Header />
+                {props.children}  
+            </div>
+           
             <Footer />
         </div>
     )
