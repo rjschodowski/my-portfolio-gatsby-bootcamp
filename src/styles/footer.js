17cce21b57a61a36e7388a06dc60bd74be8253ed
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-11 17:37:32 -0500

    Add files via upload

---
+++ src/styles/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <p>Created by R.J. Schodowski copyright 2021</p>
+        </div>
+    )
+}
+
+export default Footer
