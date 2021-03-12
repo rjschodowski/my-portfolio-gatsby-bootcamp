c77d06399a4d8039b4946eb7e5615f38217db725
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-12 18:04:15 -0500

    Add files via upload

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql, useStaticQuery } from 'gatsby'
 
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            title
+                            date
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
+    console.log(data)
+
     return (
         <Layout>
             <h1> Posts will show up here later on.</h1>
