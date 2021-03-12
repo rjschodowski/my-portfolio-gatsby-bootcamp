c3f5be265469944f88fb8df51578f87525734de4
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-12 11:02:47 -0500

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  
+  siteMetadata: {
+    title: 'R.J. Schodowski',
+    author: 'R.J. Schodowski'
+  },
+  plugins: [
+    'gatsby-plugin-sass'
+  ],
 }
