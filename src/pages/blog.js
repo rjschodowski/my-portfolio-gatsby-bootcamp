5fa1a6215b0190c8f4e522338744b0ac359cd47f
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-17 17:34:01 -0400

    Add files via upload

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Layout from '../components/layout'
 import { Link, graphql, useStaticQuery } from 'gatsby'
+import { posts } from './blog.module.scss'
+import { post } from './blog.module.scss'
+
 
 const BlogPage = () => {
     const data = useStaticQuery(graphql`
@@ -26,10 +29,10 @@ const BlogPage = () => {
     return (
         <Layout>
             <h1>Blog</h1>
-            <ol>
+            <ol className={posts}>
                 {data.allMarkdownRemark.edges.map((edge) => {
                     return (
-                        <li>
+                        <li className={post}>
                             <Link to={`/blog/${edge.node.fields.slug}`}>
                                 <h2>{edge.node.frontmatter.title}</h2>
                                 <p>{edge.node.frontmatter.date}</p>
