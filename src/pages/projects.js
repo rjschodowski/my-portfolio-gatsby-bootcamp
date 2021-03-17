60eefc31de73635c4b763c09b9acfc6bfc9fb867
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-17 18:33:50 -0400

    Add files via upload

---
+++ src/pages/projects.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const Projects = () => {
+    return (
+        <Layout>
+            <h1>SOS Lansing</h1>
+            <p>For now this is the space where a description of SOS Lansing will be.</p>
+<p><Link to="/contact">Want to work with me? Reach out</Link>.</p>
+        </Layout>
+    )
+}
+
+export default Projects
