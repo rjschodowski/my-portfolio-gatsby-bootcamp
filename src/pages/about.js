2fd899cec47e5573c2d6b1e1a20bac7b826557ea
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-17 18:35:08 -0400

    Add files via upload

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,11 +5,11 @@ import Layout from '../components/layout'
 const About = () => {
     return (
         <Layout>
-            <h1> About Me</h1>
+            <h1>Wielder of Technology for the Good of Humanity</h1>
+            <p>Hey there, I'm R.J.! I get major fulfillment in coding projects that have a positive impact on people and people groups.</p>
             <p>I am a React front-end developer currently learning about and creating excellent user experiences with 
                 React, Gatsby, Tailwind CSS, and other modern technologies. Currently seeking full-time employment at a 
-                people-focused company where I can contribute my skills and continue to grow.
-</p>
+                people-focused company where I can contribute my skills and continue to grow.</p>
 <p><Link to="/contact">Want to work with me? Reach out</Link>.</p>
         </Layout>
     )
