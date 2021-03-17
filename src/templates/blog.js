d924d201b1f11bb099f3947ae08ea62ec42af080
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-17 16:32:07 -0400

    Add files via upload

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
-const query = graphql`
+export const query = graphql`
     query($slug: String!) {
         markdownRemark(fields: { slug: { eq: $slug } }) {
             frontmatter {
@@ -14,10 +14,12 @@ const query = graphql`
     }
 `
 
-const Blog = () => {
+const Blog = (props) => {
     return (
         <Layout>
-            This is the blog template
+            <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+            <p>{props.data.markdownRemark.frontmatter.date}</p>
+            <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
         </Layout>
 
     )
